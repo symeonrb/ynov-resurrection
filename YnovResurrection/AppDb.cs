cf950c774e21524893b819c6321ddbd24b15a474
Romain Neil <tutosfaciles48@gmail.com>
2024-02-23 17:07:29 +0100

    feat: app db contexts

---
+++ YnovResurrection/AppDb.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YnovResurrection.Models;
+
+namespace YnovResuction;
+
+public class AppDb : DbContext
+{
+    
+    public DbSet<School> Schools { get; set; }
+    
+    public DbSet<Building> Buildings { get; set; }
+    
+    public DbSet<Room> Rooms { get; set; }
+    
+    public DbSet<Equipment> Equipments { get; set; }
+    
+    public DbSet<Course> Courses { get; set; }
+    
+    public DbSet<User> Users { get; set; }
+    
+    public DbSet<StudentGroup> StudentGroups { get; set; }
+    
+}
