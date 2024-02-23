ccf2dd289e9ee91ddac412b6d1aa4a4a42d2141f
Romain Neil <tutosfaciles48@gmail.com>
2024-02-23 18:36:35 +0100

    feat: base course service

---
+++ YnovResurrection/Services/CourseService.cs
@@ -0,0 +1,21 @@
+﻿using YnovResurrection.Models;
+
+namespace YnovResurrection.Services;
+
+public class CourseService : AService
+{
+
+    public void SetTeacher(Course course, string teacherId)
+    {
+        var user = _appDb.Users.SingleOrDefault(user => user.Id == teacherId);
+
+        if (user == null)
+        {
+            throw new Exception("Teacher were not found");
+        }
+
+        course.Teacher = user;
+        Flush();
+    }
+    
+}
