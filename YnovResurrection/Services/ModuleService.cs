e21b544b48c8d6d4415316c4e6423d39330e1f56
Romain Neil <tutosfaciles48@gmail.com>
2024-03-01 15:26:46 +0100

    refactor: rename course service to module

---
+++ YnovResurrection/Services/ModuleService.cs
@@ -0,0 +1,37 @@
+﻿using YnovResurrection.Models;
+
+namespace YnovResurrection.Services;
+
+public class ModuleService : AService
+{
+
+    /// <summary>
+    /// Set module teacher
+    /// </summary>
+    /// <param name="module">Module</param>
+    /// <param name="teacherId">User id of teacher</param>
+    /// <exception cref="Exception">In case teacher doesn't exist</exception>
+    public void SetTeacher(Module module, string teacherId) //TODO: rework this
+    {
+        var teacher = _appDb.Users.SingleOrDefault(user => user.Id == teacherId);
+
+        if (teacher == null)
+        {
+            throw new Exception("Teacher were not found");
+        }
+
+        SetTeacher(module, teacher);
+    }
+
+    /// <summary>
+    /// Set module teacher
+    /// </summary>
+    /// <param name="module">Module</param>
+    /// <param name="teacher">Teacher</param>
+    public void SetTeacher(Module module, User teacher)
+    {
+        module.Teacher = teacher;
+        Flush();
+    }
+    
+}
