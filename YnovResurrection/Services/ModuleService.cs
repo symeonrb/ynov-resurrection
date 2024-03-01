5d51ee5cfcd97f555e358ec33339c8ee3bed507c
Romain Neil <tutosfaciles48@gmail.com>
2024-03-01 15:35:32 +0100

    feat: function to create module

--- YnovResurrection/Services/ModuleService.cs
+++ YnovResurrection/Services/ModuleService.cs
@@ -6,6 +6,30 @@ public class ModuleService : AService
 {
 
     /// <summary>
+    /// Create a module, and return it
+    /// </summary>
+    /// <param name="isRemote"></param>
+    /// <param name="name"></param>
+    /// <param name="neededEquipments"></param>
+    /// <param name="allowSharedRoom"></param>
+    public Module CreateModule(bool isRemote, string name, string neededEquipments, bool allowSharedRoom)
+    {
+        var module = new Module
+        {
+            IsRemote = isRemote,
+            Name = name,
+            NeededEquipment = neededEquipments,
+            AllowSharedRoom = allowSharedRoom
+        };
+
+        _appDb.Modules.Add(module);
+        Flush();
+
+        return module;
+    }
+    
+
+    /// <summary>
     /// Set module teacher
     /// </summary>
     /// <param name="module">Module</param>
