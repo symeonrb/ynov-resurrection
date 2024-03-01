de732b00c81c966af83b2a8cfdd24b425ee3b0a7
Romain Neil <tutosfaciles48@gmail.com>
2024-03-01 12:22:46 +0100

    wip: user service

---
+++ YnovResurrection/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using YnovResurrection.Models;
+
+namespace YnovResurrection.Services;
+
+public class UserService : AService
+{
+
+    //TODO
+    public void CreateUser(string username, string email, string password)
+    {
+        var user = new User
+        {
+            Username = username,
+            Email = email,
+            //Password = 
+        };
+        
+        ApplyId(ref user);
+        
+        _appDb.Add(user);
+        Flush();
+    }
+    
+}
