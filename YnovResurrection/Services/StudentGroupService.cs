861a5386b18af2acd3694549d1d5ab68f806f3b1
Romain Neil <tutosfaciles48@gmail.com>
2024-03-08 13:52:24 +0100

    feat: base student group service

--- YnovResurrection/Services/StudentGroupService.cs
+++ YnovResurrection/Services/StudentGroupService.cs
@@ -1,6 +1,39 @@
-﻿namespace YnovResurrection.Services;
+﻿using YnovResurrection.Models;
+
+namespace YnovResurrection.Services;
 
 public class StudentGroupService : AService
 {
+
+    /// <summary>
+    /// Create and return a new student group
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public StudentGroup CreateStudentGroup(string name)
+    {
+        var group = new StudentGroup
+        {
+            Name = name
+        };
+        
+        ApplyId(ref group);
+        _appDb.StudentGroups.Add(group);
+        
+        Flush();
+
+        return group;
+    }
+    
+    /// <summary>
+    /// Add the specified student to the group
+    /// </summary>
+    /// <param name="group"></param>
+    /// <param name="student"></param>
+    public void AddStudent(StudentGroup group, User student)
+    {
+        group.Students.Add(student);
+        Flush();
+    }
     
 }
