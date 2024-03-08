7a218e562f4e03562e364ac225a47b060e1070b2
Necati UNLU <necati.unlu@etu.univ-nantes.fr>
2024-03-08 10:42:00 +0100

    feat: Ajout de la page d'accueil

---
+++ YnovResurection/Views/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YnovResurection
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly int namo = 5;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainFrame.Navigate(new MainMenu());
+        }
+
+        //private void Menu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    // Récupérer l'élément de menu sélectionné
+        //    ListBoxItem selectedItem = (sender as ListBox).SelectedItem as ListBoxItem;
+        //    if (selectedItem != null)
+        //    {
+        //        // Naviguer vers la page correspondante
+        //        switch (selectedItem.Content.ToString())
+        //        {
+        //            case "Accueil":
+        //                mainFrame.Navigate(new MainMenu());
+        //                break;
+        //        }
+        //    }
+        //}
+
+        private void Accueil_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new MainMenu());
+        }
+    }
+}
---
+++ YnovResurection/Views/Pages/MainMenu.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YnovResurection
+{
+    /// <summary>
+    /// Logique d'interaction pour MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        MainWindow MainWindow { get => Application.Current.MainWindow as MainWindow; }
+
+        private void Menu1_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.Navigate(new MainMenu());
+        }
+
+        private void Menu2_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.Navigate(new MainMenu());
+        }
+
+        private void Menu3_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.Navigate(new MainMenu());
+        }
+
+        private void Menu4_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.Navigate(new MainMenu());
+        }
+    }
+}
