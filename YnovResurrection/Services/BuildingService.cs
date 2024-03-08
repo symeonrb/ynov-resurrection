85262c30e79ef64d465e739dbe68e9bebb18454c
Romain Neil <tutosfaciles48@gmail.com>
2024-03-08 10:44:08 +0100

    feat: create building

---
+++ YnovResurrection/Services/BuildingService.cs
@@ -0,0 +1,20 @@
+﻿using YnovResurrection.Models;
+
+namespace YnovResurrection.Services;
+
+public class BuildingService : AService
+{
+
+    public void CreateBuilding(string address, School school)
+    {
+        var building = new Building
+        {
+            Address = address,
+            School = school
+        };
+        
+        ApplyId(ref building);
+        Flush();
+    }
+    
+}
