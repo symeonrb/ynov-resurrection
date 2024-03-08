a8e6890138cae584984d23aa5cf312c8f35b76f1
Romain Neil <tutosfaciles48@gmail.com>
2024-03-08 14:44:07 +0100

    feat: create equipment function

--- YnovResurrection/Services/EquipmentService.cs
+++ YnovResurrection/Services/EquipmentService.cs
@@ -1,6 +1,23 @@
-﻿namespace YnovResurrection.Services;
+﻿using YnovResurrection.Models;
+
+namespace YnovResurrection.Services;
 
 public class EquipmentService : AService
 {
-    //
+
+    public Equipment CreateEquipment(string type, string tags)
+    {
+        var e = new Equipment
+        {
+            Type = type,
+            Tags = tags
+        };
+        
+        ApplyId(ref e);
+        _appDb.Equipments.Add(e);
+        Flush();
+
+        return e;
+    }
+    
 }
