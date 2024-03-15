b7059bda117945521ce2f98ea5681b3f4ca0608e
Syméon ROUGEVIN <symeon@oclocher.fr>
2024-03-15 16:55:14 +0100

    Added SchoolsPage

--- YnovResurrection/Views/Pages/SideMenuPage.xaml.cs
+++ YnovResurrection/Views/Pages/SideMenuPage.xaml.cs
@@ -15,24 +15,14 @@ namespace YnovResurrection.Views.Pages
 
         static MainWindow MainWindow { get => (MainWindow)Application.Current.MainWindow; }
 
+        private void Schools_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.schoolsPage.Navigate(new SchoolsPage());
+        }
+
         private void Buildings_Click(object sender, RoutedEventArgs e)
         {
             MainWindow.buildingsPage.Navigate(new BuildingsPage());
-        }
-
-        private void Menu2_Click(object sender, RoutedEventArgs e)
-        {
-            //mainFrame.Navigate(new MainMenu());
-        }
-
-        private void Menu3_Click(object sender, RoutedEventArgs e)
-        {
-            //mainFrame.Navigate(new MainMenu());
-        }
-
-        private void Menu4_Click(object sender, RoutedEventArgs e)
-        {
-            //mainFrame.Navigate(new MainMenu());
         }
     }
 }
