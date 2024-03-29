d18f12f2ba7fe90b86c844800547d7bd0c0a623c
Romain Neil <contact@romain-neil.fr>
2024-03-29 13:34:16 +0100

    feat: add course attribution model

---
+++ YnovResurrection/Models/CourseAttribution.cs
@@ -0,0 +1,11 @@
+﻿namespace YnovResurrection.Models;
+
+public class CourseAttribution : IModel
+{
+    public string Id { get; set; }
+    
+    public int CourseId { get; set; }
+    
+    public int RoomId { get; set; }
+    
+}
