17a4f68c38d9291ee8c1ef95219701808d4e6ef2
Syméon ROUGEVIN <symeon@oclocher.fr>
2024-04-13 00:22:59 +0200

    Added RoomsListPageViewModel

---
+++ YnovResurrection/ViewModels/Pages/RoomsListPageViewModel.cs
@@ -0,0 +1,38 @@
+using System.Windows.Controls;
+using YnovResurrection.Models;
+using YnovResurrection.Services;
+using YnovResurrection.Views.Pages;
+
+namespace YnovResurrection.ViewModels.Pages
+{
+    class RoomsListPageViewModel : IModelListPageViewModel
+    {
+        public Type ModelType => typeof(Room);
+        public string Title => "Gestion des Salles";
+
+        public Page? EditModel(ModelListPage page, IModel? modelNullable)
+        {
+            return null;
+            // TODO
+            // var roomNullable = modelNullable ?? new Room();
+            // if (roomNullable is not Room room) return null;
+            // RoomPageViewModel editViewModel = new(room)
+            // {
+            //     Page = page,
+            //     IsEditMode = modelNullable != null,
+            //     IsAddMode = modelNullable == null
+            // };
+            // RoomPage editPage = new(editViewModel);
+            // return editPage;
+        }
+
+        public void DeleteModel(IModel model)
+        {
+            // TODO
+            // if (model is not Room room) return;
+            // RoomService.Instance.DeleteRoom(room);
+        }
+
+        public ICollection<Room> List => RoomService.Instance.List();
+    }
+}
--- YnovResurrection/Views/Pages/SideMenuPage.xaml.cs
+++ YnovResurrection/Views/Pages/SideMenuPage.xaml.cs
@@ -28,20 +28,16 @@ namespace YnovResurrection.Views.Pages
             MainWindow.MainPage.Navigate(page);
         }
 
+        private void Rooms_Click(object sender, RoutedEventArgs e)
+        {
+            ModelListPage page = new(new RoomsListPageViewModel());
+            MainWindow.MainPage.Navigate(page);
+        }
+
         private void Courses_Click(object sender, RoutedEventArgs e)
         {
             ModelListPage page = new(new CoursesListPageViewModel());
             MainWindow.MainPage.Navigate(page);
-        }
-
-        private void Menu3_Click(object sender, RoutedEventArgs e)
-        {
-            //mainFrame.Navigate(new MainMenu());
-        }
-
-        private void Menu4_Click(object sender, RoutedEventArgs e)
-        {
-            //mainFrame.Navigate(new MainMenu());
         }
     }
 }
